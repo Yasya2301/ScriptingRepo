661b49a90222084a37263bad54f1dffd0d891fa4
Yasya <ivannikovyura1984@gmail.com>
2026-05-16 13:12:59 +0300

    Первый комит

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Вывод в консоль приветствия миру
+        print("Hello, Worid!");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
